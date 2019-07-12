4b985e7fbd1e46e0bf678f70d7b14af03618c7c6
David Quiñones <daquinons@users.noreply.github.com>
2019-07-12 10:26:54 +0200

    Add endpoint to retrieve projects actions

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const projectsController = require('../controllers/projectsController');
+const actionsController = require('../controllers/actionsController');
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -12,5 +13,9 @@ router.post('/projects', projectsController.post);
 router.get('/projects/:id', projectsController.get);
 router.delete('/projects/:id', projectsController.delete);
 router.put('/projects/:id', projectsController.put);
+router.get('/projects/:id/actions', projectsController.getActions);
+
+// Actions
+router.get('/actions', actionsController.getAll);
 
 module.exports = router;
