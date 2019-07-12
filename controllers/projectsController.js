7487e449d95202ae962b31d352a77d672f4c7a48
David Quiñones <daquinons@users.noreply.github.com>
2019-07-12 10:14:48 +0200

    Add project router completed

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,20 +1,71 @@
 const Projects = require('../data/helpers/projectModel');
 
-exports.get = async (req, res) => {
+exports.getAll = async (req, res) => {
   try {
     const projects = await Projects.get();
     res.json(projects);
   } catch (error) {
-    res.status(500).json({ message: 'There was an error retrieving the projects'});
+    res
+      .status(500)
+      .json({ message: 'There was an error retrieving the projects' });
+  }
+};
+
+exports.get = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const project = await Projects.get(id);
+    res.json(project);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'There was an error retrieving the projects' });
   }
 };
 
 exports.post = async (req, res) => {
   try {
-    const project = req.body
+    const project = {
+      name: req.body.name,
+      description: req.body.description,
+      completed: req.body.completed
+    };
     const newProject = await Projects.insert(project);
     res.status(201).json(newProject);
   } catch (error) {
-    res.status(500).json({ message: 'There was a problem inserting your post' });
+    res
+      .status(500)
+      .json({ message: 'There was a problem inserting your post' });
   }
-}
+};
+
+exports.put = async (req, res) => {
+  try {
+    const project = {
+      name: req.body.name,
+      description: req.body.description,
+      completed: req.body.completed
+    };
+    const id = req.params.id;
+    const updatedProject = await Projects.update(id, project);
+    res.json(updatedProject);
+  } catch (error) {
+    res.status(500).json({ message: 'There was a problem updating your post' });
+  }
+};
+
+exports.delete = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const projectsDeleted = await Projects.remove(id);
+    if (projectsDeleted) {
+      res.json({
+        message: `The project with id ${id} was successfully deleted`
+      });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'There was an error inserting your project' });
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,14 @@ const projectsController = require('../controllers/projectsController');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.json({ message: 'API is up'});
-})
+  res.json({ message: 'API is up' });
+});
 
 // Projects
-router.get('/projects', projectsController.get);
+router.get('/projects', projectsController.getAll);
 router.post('/projects', projectsController.post);
+router.get('/projects/:id', projectsController.get);
+router.delete('/projects/:id', projectsController.delete);
+router.put('/projects/:id', projectsController.put);
 
 module.exports = router;
