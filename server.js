02edc712967bebd804cb9142dd4902fc96f682bc
David Quiñones <daquinons@users.noreply.github.com>
2019-07-12 12:01:27 +0200

    Install cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 const routes = require('./routes');
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 server.use(helmet());
 
 server.use('/api', routes);
